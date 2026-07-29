8767a176ddffd94926a9e18b8c6694c25b45d56b
tanmay <tan2018carlson@gmail.com>
2026-07-30 00:49:10 +0530

    student management app code uploaded

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    # return "<h1>Welcome to Student Management System</h1>"
+    # return "<h1>Hello Tanmay! 🚀</h1>"
+    return render_template("index.html")
+
+@app.route("/students")
+def students():
+    return render_template("students.html")
+
+
+@app.route("/add-student")
+def add_student():
+    return render_template("add_student.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
