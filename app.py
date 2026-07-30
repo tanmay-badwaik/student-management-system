ee4c5870619bb9b67a7c40ca9def130d69549c96
tanmay <tan2018carlson@gmail.com>
2026-07-30 21:41:32 +0530

    network binding updated

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.register_blueprint(student_bp)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 # from flask import Flask,render_template
