6ecd58b4912a43521c851f81cb8b84ad7fbfda93
tanmay <tan2018carlson@gmail.com>
2026-07-30 23:23:10 +0530

    add student and routes updated

--- routes/student.py
+++ routes/student.py
@@ -1,13 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from models import db
+from models.student import Student
 
-student_bp = Blueprint("student", __name__)
-
-
-@student_bp.route("/students")
-def students():
-    return render_template("students.html")
+student_bp = Blueprint("student", __name__, url_prefix="/students")
 
 
-@student_bp.route("/add-student")
+@student_bp.route("/add", methods=["GET", "POST"])
 def add_student():
-    return render_template("add_student.html")  
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        course = request.form["course"]
+
+        student = Student(
+            name=name,
+            email=email,
+            course=course
+        )
+
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for("student.add_student"))
+
+    return render_template("add_student.html")
