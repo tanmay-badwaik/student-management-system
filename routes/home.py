16a8a5db23b97032a8d87730c476d74cd493c547
tanmay <tan2018carlson@gmail.com>
2026-08-01 16:58:16 +0530

    dashboard cards added

--- routes/home.py
+++ routes/home.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template
 
+from models.student import Student
+from models.user import User
+
 home_bp = Blueprint("home", __name__)
 
 
 @home_bp.route("/")
 def home():
-    return render_template("index.html")
+
+    total_students = Student.query.count()
+
+    total_users = User.query.count()
+
+    return render_template(
+        "index.html",
+        total_students=total_students,
+        total_users=total_users
+    )
