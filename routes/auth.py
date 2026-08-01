e6580af65e6c8ef4f868733ef6f8451d3d9ef462
tanmay <tan2018carlson@gmail.com>
2026-08-01 15:40:27 +0530

    login with password updated

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,16 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    session
+)
 
+from werkzeug.security import check_password_hash
+
+from models.user import User
 auth_bp = Blueprint(
     "auth",
     __name__,
@@ -7,7 +18,38 @@ auth_bp = Blueprint(
 )
 
 
-@auth_bp.route("/login")
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            username=username
+        ).first()
+
+        if user and check_password_hash(
+            user.password,
+            password
+        ):
+
+            session["user_id"] = user.id
+            session["username"] = user.username
+
+            flash(
+                "Login Successful!",
+                "success"
+            )
+
+            return redirect(
+                url_for("student.students")
+            )
+
+        flash(
+            "Invalid Username or Password!",
+            "danger"
+        )
 
     return render_template("login.html")
